3c7ac0c66dc7e1dcab0724c4c8bf7a6ab3d40129
james <jshortall@student.bridgew.edu>
2022-09-19 18:58:34 -0400

    night week 3

---
+++ night week 3.py
@@ -0,0 +1,6 @@
+best_class= input("What is your favorite class ")
+loud_version = best_class.upper()
+#lets down case the string
+lower_version= best_class. lower ()
+print(f" Your favorite class is {loud_version}")
+print(f" and the lower case version is {lower_version}")
